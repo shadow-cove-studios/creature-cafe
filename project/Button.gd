643c37e18337f4733ec929b50935ff9186aa71b5
Voxel_Cat <117233448+Voxel-Cat@users.noreply.github.com>
2024-03-12 19:29:08 -0700

    move
    added (temporary) movement system

--- project/Button.gd
+++ project/Button.gd
@@ -1,11 +1,11 @@
-extends Button
+extends CharacterBody2D
 
+@export var speed = 400
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
